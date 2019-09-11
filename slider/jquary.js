acd3a75cf51f2c05d0d2cd1e0e197dac73cb1103
unknown <karankumarnepal7@gmail.com>
2019-09-11 13:45:19 +0545

    first commit

---
+++ slider/jquary.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+document.getElementById("menu").onclick = function(){
+    document.getElementById("menu_wrapper").classList.toggle("show");
+}
---
+++ slider1/jquary.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+document.getElementById("menu").onclick = function(){
+    document.getElementById("menu_wrapper").classList.toggle("show");
+}
